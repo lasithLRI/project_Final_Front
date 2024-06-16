38f31a7b5c9cc21b91c9c4f203535c9eaebfb1a0
Ridmike <ridmikew@gmail.com>
2024-06-16 23:41:23 +0530

    login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,4 +3,5 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {path:'',redirectTo:'/ads',pathMatch:"full"},
   {path:'ads',loadComponent:()=>import('./components/ads/ads.component').then(c=>c.AdsComponent)},
+  {path:'register',loadComponent:()=>import('./components/signup/signup.component').then(c=>c.SignupComponent)},
 ];
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from '@angular/core';
+import {Button} from "primeng/button";
+import {DialogModule} from "primeng/dialog";
+import {InputTextModule} from "primeng/inputtext";
+import {FormsModule} from "@angular/forms";
+import {PaginatorModule} from "primeng/paginator";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    Button,
+    DialogModule,
+    InputTextModule,
+    FormsModule,
+    PaginatorModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit{
+
+  email: string = '';
+  password: string = '';
+
+  visible: boolean = false;
+
+  showDialog() {
+    this.visible = true;
+  }
+
+  closeDialog() {
+    this.visible = false;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
