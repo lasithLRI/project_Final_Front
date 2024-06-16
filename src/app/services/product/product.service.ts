52ed601469a26d153bad62c0bebaf23c835ac48d
lasithLRI <lasith.hettiarachchi.dev@gmail.com>
2024-06-16 14:49:07 +0530

    dev product dist added

--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {ProductsComponent} from "../../components/products/products.component";
+
 import {PostModal} from "../../modals/Post.modal";
 
 @Injectable({
