21f8d7255eb58d21cfb762e2257dfea99e66150e
lasithLRI <lasith.hettiarachchi.dev@gmail.com>
2024-06-16 19:06:57 +0530

    add product route created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,4 +4,5 @@ export const routes: Routes = [
   {path:'',redirectTo:'/ads',pathMatch:"full"},
   {path:'ads',loadComponent:()=>import('./components/ads/ads.component').then(c=>c.AdsComponent)},
   {path:'register',loadComponent:()=>import('./components/signup/signup.component').then(c=>c.SignupComponent)},
+  {path:'add',loadComponent:()=>import('./components/post-ad-cat/post-ad-cat.component').then(c=>c.PostAdCatComponent)},
 ];
