e94a172137a9b4e6acee0aad12eeb215a4e5d7d9
Ridmike <ridmikew@gmail.com>
2024-06-17 20:18:58 +0530

    login form

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
 import {RouterLink} from "@angular/router";
+import {LoginComponent} from "../login/login.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     MatIcon,
-    RouterLink
+    RouterLink,
+    LoginComponent
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
