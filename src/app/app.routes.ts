3b0c176c1d3db23974d3b2757c4dbc898a65ea7a
Ridmike <ridmikew@gmail.com>
2024-07-21 12:12:16 +0530

    register done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,10 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {path:'',redirectTo:'/ads',pathMatch:"full"},
   {path:'ads',loadComponent:()=>import('./components/ads/ads.component').then(c=>c.AdsComponent)},
-  {path:'register',loadComponent:()=>import('./components/signup/signup.component').then(c=>c.SignupComponent)},
+  {path:'register',loadComponent:()=>import('./components/register/register.component').then(c=>c.RegisterComponent)},
   {path:'cat',loadComponent:()=>import('./components/post-ad-cat/post-ad-cat.component').then(c=>c.PostAdCatComponent)},
   {path:'form',loadComponent:()=>import('./components/post-ad-form/post-ad-form.component').then(c=>c.PostAdFormComponent)},
   {path:'login',loadComponent:()=>import('./components/login/login.component').then(c=>c.LoginComponent)},
-  {path:'home',loadComponent:()=>import('./components/home/home.component').then(c=>c.HomeComponent)}
-
+  {path:'home',loadComponent:()=>import('./components/home/home.component').then(c=>c.HomeComponent)},
+  {path:'contactus',loadComponent:()=>import('./components/contact-us/contact-us.component').then(c=>c.ContactUsComponent)},
 ];
